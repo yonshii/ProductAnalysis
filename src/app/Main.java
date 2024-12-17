50f83d97cc38221dc8ff0acfe96ca470bdcd667d
yonshii <dkaranets2@gmail.com>
2024-12-17 19:18:13 +0100

    Change output, add category

--- src/app/Main.java
+++ src/app/Main.java
@@ -9,20 +9,30 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         List<Product> products = Arrays.asList(
-                new Product("Laptop", "Electronics", 1300),
-                new Product("Headphones", "Electronics",200),
-                new Product("Coffee Maker", "Appliances", 150),
-                new Product("Telephone", "Electronics", 500),
+                new Product("Laptop", "Electronics", 1399),
+                new Product("Headphones", "Electronics", 210),
+                new Product("Coffee Maker", "Appliances", 155),
+                new Product("Telephone", "Electronics", 530),
                 new Product("Blender", "Appliances", 135),
-                new Product("Mixer", "Appliances", 200)
+                new Product("Mixer", "Appliances", 272),
+                new Product("Shovel", "Garden Supplies", 27),
+                new Product("Garden pruner", "Garden Supplies", 15),
+                new Product("lawnmower", "Garden Supplies", 599)
         );
 
-        Map<String, Double> averagePrices = products.stream().collect(Collectors.groupingBy(Product::getCategory, Collectors.averagingDouble(Product::getPrice)));
+        Map<String, Double> averagePrices = products.stream()
+                .collect(Collectors.groupingBy(Product::getCategory, Collectors.averagingDouble(Product::getPrice)));
 
-        System.out.println("The average price of products by category: " + averagePrices);
+        System.out.println("The average price of products by category:");
+        averagePrices.forEach((category, avgPrice) ->
+                System.out.printf("Category: %s -> Average Price: %.2f%n", category, avgPrice));
 
-        Optional<Map.Entry<String, Double>> maxCategory = averagePrices.entrySet().stream().max(Map.Entry.comparingByValue());
+        Optional<Map.Entry<String, Double>> maxCategory = averagePrices.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue());
 
-        maxCategory.ifPresent(entry -> System.out.println("The category with the highest average price: " +  entry.getKey() + ", with an average price:  " + entry.getValue()));
+        maxCategory.ifPresent(entry ->
+                System.out.printf("The category with the highest average price: %s -> Average Price: %.2f%n",
+                        entry.getKey(), entry.getValue()));
     }
 }
